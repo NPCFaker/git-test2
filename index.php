d49acc78cd6196e67cd1d8ebebeaf234a079232e
NPCFaker <derrydavinci@gmail.com>
2021-10-04 22:08:36 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>HELLO WORLD!!</h1>
     <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum perferendis exercitationem dignissimos non accusantium unde ipsam, officia quam inventore. Molestiae labore non blanditiis saepe eius, accusamus provident accusantium ipsam debitis?</h4>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>

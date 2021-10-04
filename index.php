4c643efed9c620b310c2b717e59b68b2241f862b
NPCFaker <derrydavinci@gmail.com>
2021-10-04 21:55:56 +0700

    menambah pragraph

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba github2</title>
+</head>
+<body>
+    <h1>HELLO WORLD!!</h1>
+    <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum perferendis exercitationem dignissimos non accusantium unde ipsam, officia quam inventore. Molestiae labore non blanditiis saepe eius, accusamus provident accusantium ipsam debitis?</h4>
+</body>
+</html>
